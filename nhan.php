b38901392a7758f06fa3138ba2823b789693014d
DNhan1610 <dinhthinhan16102004@gmail.com>
2024-08-20 21:54:08 +0700

    Add files via upload

---
+++ nhan.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <title>Đinh Thị Nhạn</title>
+</head>
+<body>
+    <h1>BTVN</h1>
+    <?php
+    $message = "Chào mừng bạn đến với trang web của chúng tôi!";
+    ?>
+
+    <p><?php echo $message; ?></p>
+</body>
+</html>
